3e8c9c4188bdd4a5d5bf231e4a243fe09a057597
gia khanh <dracura1997@gmail.com>
2019-11-18 12:37:19 +0900

    ss

--- zaiko_ichiran.php
+++ zaiko_ichiran.php
@@ -83,6 +83,8 @@
 						//⑩SQLの実行結果の変数から1レコードのデータを取り出す。レコードがない場合はループを終了する。
 						// while(/* ⑩の処理を書く */){
 							//⑪extract変数を使用し、1レコードのデータを渡す。
+							
+							
 
 							// echo "<tr id='book'>";
 							// echo "<td id='check'><input type='checkbox' name='books[]'value="./* ⑫IDを設定する */."></td>";
